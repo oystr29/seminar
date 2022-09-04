e3b44ae94cb812cb9a9037c1e64121f96bbb613a
oktoala <okta.yoga.syah@gmail.com>
2022-09-04 15:21:44 +0800

    Add install button

--- pages/_app.js
+++ pages/_app.js
@@ -15,15 +15,16 @@ function MyApp({ Component, pageProps }) {
       <meta name="msapplication-TileColor" content="#9E38B3" />
       <meta name="msapplication-tap-highlight" content="no" />
       <meta name="theme-color" content="#000000" />
+      <link rel="manifest" href="/manifest.webmanifest" />
 
-      <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/icons/apple-touch-icon-57x57.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/icons/apple-touch-icon-114x114.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/icons/apple-touch-icon-72x72.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/icons/apple-touch-icon-144x144.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/icons/apple-touch-icon-60x60.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/icons/apple-touch-icon-120x120.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/icons/apple-touch-icon-76x76.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/icons/apple-touch-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/apple-touch-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/apple-touch-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/apple-touch-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/apple-touch-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/apple-touch-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/apple-touch-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/apple-touch-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-touch-icon-152x152.png" />
       <link rel="icon" type="image/png" href="/assets/icons/favicon-196x196.png" sizes="196x196" />
       <link rel="icon" type="image/png" href="/assets/icons/favicon-96x96.png" sizes="96x96" />
       <link rel="icon" type="image/png" href="/assets/icons/favicon-32x32.png" sizes="32x32" />
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,119 @@
-import useSWR from 'swr';
-import Countdown from 'react-countdown';
-import { useEffect, useState } from 'react';
-import { HiOutlineCalendar, HiOutlineClock, HiOutlineLocationMarker, HiDocument, HiPresentationChartLine } from 'react-icons/hi';
-import { MdSchool } from 'react-icons/md';
+import Countdown from "react-countdown";
+import { useEffect, useState } from "react";
+import {
+  HiOutlineCalendar,
+  HiOutlineClock,
+  HiOutlineLocationMarker,
+  HiDocument,
+  HiPresentationChartLine,
+} from "react-icons/hi";
+import { MdSchool } from "react-icons/md";
 
-// export async function getStaticProps() {
+export async function getServerSideProps() {
+  const base =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://seminar.dalamkotak.com";
+  const data = await fetch(`${base}/api/hello`);
+  const { currents, scheduled, notyet, passed } = await data.json();
 
-//   const data = await fetch(`${process.env.BASE_URL}/api/hello`);
-//   const {currents, scheduled, notyet, passed} = await data.json();
+  return {
+    props: {
+      currents,
+      scheduled,
+      notyet,
+      passed,
+    },
+  };
+}
 
-//   return {
-//     props: {
-//       currents,
-//       scheduled,
-//       notyet,
-//       passed,
-//     },
-//   }
-// }
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+export default function Home(props) {
+  // const base = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://seminar.dalamkotak.com';
+  // const url = `${base}/api/hello`;
+  // const { data, error } = useSWR(url, fetcher);
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [isInstall, setIsInstall] = useState(true);
 
-export default function Home() {
-  const base = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://seminar.dalamkotak.com';
-  const url = `${base}/api/hello`;
-  const { data, error } = useSWR(url, fetcher);
+  function listenUserAction() {
+    setIsInstall(true);
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      setDeferredPrompt(null);
+    });
+  }
 
-  if (error) return "An error has occurred.";
-  if (!data) return <Spinner />
-  const { currents, scheduled, notyet, passed } = data
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      setDeferredPrompt(e);
+      // Update UI to notify the user they can add to home screen
+      setIsInstall(false);
+    });
+  }, []);
+
+  // if (error) return "An error has occurred.";
+  // if (!data) return <Spinner />
+  const { currents, scheduled, notyet, passed } = props;
 
   return (
-    <div className='container 
-    mx-auto mt-10'>
-      {
-        currents.length !== 0 && currents.map((e, i) => {
-          return (
-            <Item e={e} key={e.nim + i} classes="current" />
-          );
-        })
-      }
-      {notyet.length !== 0 && <div className='p-1 px-2 mb-2 rounded-xl mt-3 text-base border-2 text-purple-300 border-purple-800 w-max'>Coming Soon!</div>}
-      {
-        notyet.map((e, i) => {
-          return (
-            <Item e={e} key={e.nim + i} classes="notyet" />
-          );
-        })
-      }
-      {scheduled.length !== 0 && <div className='p-1 px-2 mb-2 mt-3 rounded-xl text-base border-2 text-yellow-300 border-yellow-800 w-max'>Belum Ada Jadwalnya</div>}
-      {scheduled.map((e, i) => {
-        return (
-          <Item e={e} key={e.nim + i} classes="scheduled" />
-        );
+    <div
+      className="container 
+    mx-auto mt-10"
+    >
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold mb-5">
+          Jadwal Seminar Informatika
+        </h1>
+        <button
+          className={`${isInstall ? "hidden" : "block"} bg-gradient-to-r ${currents.length !== 0
+            ? " from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 "
+            : " from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600"
+            } text-white  rounded-lg focus:outline-none px-5 py-2 font-semibold`}
+          onClick={listenUserAction}
+        >
+          Install
+        </button>
+      </div>
+      {currents.length !== 0 &&
+        currents.map((e, i) => {
+          return <Item e={e} key={e.nim + i} classes="current" />;
+        })}
+      {notyet.length !== 0 && (
+        <div className="p-1 px-2 mb-2 rounded-xl mt-3 text-base border-2 text-purple-300 border-purple-800 w-max">
+          Coming Soon!
+        </div>
+      )}
+      {notyet.map((e, i) => {
+        return <Item e={e} key={e.nim + i} classes="notyet" />;
       })}
-      {passed.length !== 0 && <div className='p-1 px-2 mb-2 mt-3 rounded-xl text-base border-2 text-gray-300 border-gray-500 w-max'>Udah Lewat</div>}
-      {passed.map((e, i) => {
-        return (
-          <Item e={e} key={e.nim + i} classes="passed" />
-        );
-      }).reverse()}
+      {scheduled.length !== 0 && (
+        <div className="p-1 px-2 mb-2 mt-3 rounded-xl text-base border-2 text-yellow-300 border-yellow-800 w-max">
+          Belum Ada Jadwalnya
+        </div>
+      )}
+      {scheduled.map((e, i) => {
+        return <Item e={e} key={e.nim + i} classes="scheduled" />;
+      })}
+      {passed.length !== 0 && (
+        <div className="p-1 px-2 mb-2 mt-3 rounded-xl text-base border-2 text-gray-300 border-gray-500 w-max">
+          Udah Lewat
+        </div>
+      )}
+      {passed
+        .map((e, i) => {
+          return <Item e={e} key={e.nim + i} classes="passed" />;
+        })
+        .reverse()}
     </div>
   );
 }
@@ -67,67 +121,89 @@ export default function Home() {
 const Item = (props) => {
   const { e, classes } = props;
   return (
-    <div className={`items ${classes}`} >
-      <div className={`${classes === 'current' ? 'gradient' : null}`}>
+    <div className={`items ${classes}`}>
+      <div className={`${classes === "current" ? "gradient" : null}`}>
         <div className={`font-bold text-xl mb-2`}>{e.judul}</div>
-        <span className='uppercase font-semibold'>{e.nama} </span>
+        <span className="uppercase font-semibold">{e.nama} </span>
         <span> - </span>
-        <span className='font-semibold'>{e.nim}</span>
-        <div className='mb-5'>
-          {e.date.day.hari !== '' &&
-            <div className='flex items-center flex-row my-1'>
-              <HiOutlineCalendar className=' mr-2' />
-              <span>{e.date.day.hari}, {e.date.day.tanggal} - {e.date.day.bulanAsli} - {e.date.day.tahun}</span>
+        <span className="font-semibold">{e.nim}</span>
+        <div className="mb-5">
+          {e.date.day.hari !== "" && (
+            <div className="flex items-center flex-row my-1">
+              <HiOutlineCalendar className=" mr-2" />
+              <span>
+                {e.date.day.hari}, {e.date.day.tanggal} - {e.date.day.bulanAsli}{" "}
+                - {e.date.day.tahun}
+              </span>
             </div>
-          }
-          {e.date.time !== '' &&
-            <div className='flex flex-row items-center mb-1'>
-              <HiOutlineClock className='mr-2' />
-              <span>{e.date.time.jamMulai} - {e.date.time.jamAkhir} WITA</span>
+          )}
+          {e.date.time !== "" && (
+            <div className="flex flex-row items-center mb-1">
+              <HiOutlineClock className="mr-2" />
+              <span>
+                {e.date.time.jamMulai} - {e.date.time.jamAkhir} WITA
+              </span>
             </div>
-          }
-          {e.jadwal.ruang !== '' &&
-            <div className='flex flex-row items-center '>
-              <HiOutlineLocationMarker className='mr-2' />
+          )}
+          {e.jadwal.ruang !== "" && (
+            <div className="flex flex-row items-center ">
+              <HiOutlineLocationMarker className="mr-2" />
               <span>{e.jadwal.ruang}</span>
             </div>
-          }
-          {e.sempro &&
-            <div className='flex flex-row items-center mt-4'>
-              <HiDocument className='mr-2' />
+          )}
+          {e.sempro && (
+            <div className="flex flex-row items-center mt-4">
+              <HiDocument className="mr-2" />
               <span>Seminar Proposal</span>
             </div>
-          }
-          {e.semhas &&
-            <div className='flex flex-row items-center mt-4'>
-              <HiPresentationChartLine className='mr-2' />
+          )}
+          {e.semhas && (
+            <div className="flex flex-row items-center mt-4">
+              <HiPresentationChartLine className="mr-2" />
               <span>Seminar Hasil</span>
             </div>
-          }
-          {e.pendadaran &&
-            <div className='flex flex-row items-center mt-4'>
-              <MdSchool className='mr-2' />
+          )}
+          {e.pendadaran && (
+            <div className="flex flex-row items-center mt-4">
+              <MdSchool className="mr-2" />
               <span>Seminar Akhir</span>
             </div>
-          }
+          )}
         </div>
-
-        {Date.now() <= e.dateInt.akhir && classes === 'current' &&
+        {Date.now() <= e.dateInt.akhir && classes === "current" && (
           <Countdown date={e.dateInt.akhir} className="font-semibold" />
-        }
-        {Date.now() <= e.dateInt.mulai && classes === 'notyet' &&
+        )}
+        {Date.now() <= e.dateInt.mulai && classes === "notyet" && (
           <Countdown date={e.dateInt.mulai} className="font-semibold" />
-        }
+        )}
       </div>
     </div>
   );
-}
+};
 
 const Spinner = () => {
-  return <div className='flex justify-center items-center  '>
-    <svg className="animate-spin  text-white h-20 w-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-    </svg>
-  </div>
-}
+  return (
+    <div className="flex justify-center items-center  ">
+      <svg
+        className="animate-spin  text-white h-20 w-20"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
