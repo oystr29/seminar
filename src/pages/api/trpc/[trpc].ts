eca6c3dfeb86e043cdb126f890c383f3fa06afbc
oktoala <okta.yoga.syah@gmail.com>
2024-08-26 22:03:34 +0800

    test cors

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,52 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/root";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+function getBaseUrl() {
+  if (typeof window !== "undefined")
+    // browser should use relative path
+    return "";
+
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    // reference for render.com
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
 
 // export API handler
 // @see https://trpc.io/docs/api-handler
-export default trpcNext.createNextApiHandler({
+const nextApiHandler = trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => ({}),
 });
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  /*   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*"); */
+  // If you need to make authenticated CORS calls then
+  // remove what is above and uncomment the below code
+  // Allow-Origin has to be set to the requesting domain that you want to send the credentials back to
+  res.setHeader("Access-Control-Allow-Origin", getBaseUrl());
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Referrer-Policy", "no-referrer");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    return res.end();
+  }
+  // finally pass the request on to the tRPC handler
+  return nextApiHandler(req, res);
+}
